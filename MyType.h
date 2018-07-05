1805ac3f2cd1fe3b56062d57568ee76c20f2f953
David Chen <david87124@gmail.com>
2018-07-05 16:04:03 +0800

    Update MyType.h

--- MyType.h
+++ MyType.h
@@ -1,22 +1,28 @@
-typedef	unsigned char u8;
-typedef	char s8;
-typedef	unsigned short u16;
-typedef	short s16;
-typedef	unsigned long	u32;
-typedef	long s32;
-#define SEG_Port	_pc
-#define	SEG_PortC	_pcc
-#define	  d0  _pd0
-#define   dC0 _pdc0
-#define	  d1  _pd1
-#define   dC1 _pdc1
-#define	  d2  _pd2
-#define   dC2 _pdc2
-#define	  d3  _pd3
-#define   dC3 _pdc3
-#define 	SPK_Port  _pd4			
-#define 	SPK_PortC _pdc4			
-#define 	KeyPort   _pe
-#define 	KeyPortC  _pec
-#define 	KeyPortPU _pepu
-#define   _1M 1000000								
+typedef unsigned char u8;
+typedef char s8;
+typedef unsigned short u16;
+typedef short s16;
+typedef unsigned long u32;
+typedef long s32;
+
+#define SEG_Port _pc
+#define SEG_PortC _pcc
+
+#define d0 _pd0
+#define d1 _pd1
+#define d2 _pd2
+#define d3 _pd3
+
+#define dC0 _pdc0
+#define dC1 _pdc1
+#define dC2 _pdc2
+#define dC3 _pdc3
+
+#define SPK_Port _pd4			
+#define SPK_PortC _pdc4
+
+#define KeyPort _pe
+#define KeyPortC _pec
+#define KeyPortPU _pepu
+
+#define _1M 1000000								
